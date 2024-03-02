c654467749e5d97f3824e9618e553991d2b7d0a9
Tim Beatham <tcb1@st-andrews.ac.uk>
2024-03-02 21:06:41 +0000

    word break

--- visualisation/TennentsFlow.js
+++ visualisation/TennentsFlow.js
@@ -9,6 +9,36 @@ function quaternionFromYAngle(angle) {
     return quat.setFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), angle);
 }
 
+export function formatStringIndent(inputString, maxCharacters) {
+    const words = inputString.split(' ');
+
+    let resultantString = [];
+    resultantString.push("");
+
+    for (const word of words) {
+        let lastLine = resultantString[resultantString.length - 1];
+
+        if (lastLine.length >= maxCharacters) {
+            resultantString.push("");
+            lastLine = resultantString[resultantString.length - 1];
+        }
+
+        if (lastLine !== "" && lastLine.length + word.length > maxCharacters) {
+            resultantString.push("");
+            lastLine = resultantString[resultantString.length - 1];
+        }
+
+        lastLine += `${word} `;
+        resultantString[resultantString.length - 1] = lastLine;
+    }
+
+    return resultantString.map(line => {
+        const l = line.replace(/~+$/, '')
+        const padding = maxCharacters - l.length / 2;
+        return `${" ".repeat(padding)}${l}${" ".repeat(padding)}`
+    }).join("\n");
+}
+
 export default class TennentsFlow {
 
     constructor() {
@@ -147,7 +177,7 @@ export default class TennentsFlow {
         //     // sign.pos
         // });
 
-        // mixesr 
+        // mixesr
 
         this.mixers.forEach(mixer => {
             mixer.update(delta);
