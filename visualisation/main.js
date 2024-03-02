e66a6d3c5248e1d031dcb40c338f07756e849fc8
billy <rookebilly@gmail.com>
2024-03-02 13:24:33 +0000

    Create spinning cube

--- visualisation/main.js
+++ visualisation/main.js
@@ -2,6 +2,8 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
+const windowScale = 0.75;
+
 const camera = new THREE.PerspectiveCamera(
     75, // field of view
     window.innerWidth / window.innerHeight, // aspect ratio
@@ -9,15 +11,31 @@ const camera = new THREE.PerspectiveCamera(
     1000 // far clip
 );
 
-const renderer = new THREE.WebGLRenderList();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(
+    window.innerWidth  * windowScale,
+    window.innerHeight * windowScale
+);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
     color: 0x00FFFF
 });
-const cube = new THREE.Mesh(geometry.material);
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = RGBA_ASTC_5x4_Format;
+camera.position.z = 5;
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    renderer.render(scene, camera);
+
+}
+
+animate();
