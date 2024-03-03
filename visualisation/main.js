76a3cfe73d6cf13143bcaa2b9b2a5d62e281a019
billy <rookebilly@gmail.com>
2024-03-03 09:27:24 +0000

    Controllable tick rate

--- visualisation/main.js
+++ visualisation/main.js
@@ -12,11 +12,41 @@ function inc() {
     setTimeout(() => inc(), 1000);
 }
 
+let tickPeriod = 0;
+
+function setTickPeriod(period) {
+    tickPeriod = period;
+    document.querySelector("#tickRate").innerText = `Tick Rate: ${tickPeriod}`
+}
+
 if ( WebGL.isWebGLAvailable() ) {
+
+    setTickPeriod(2200);
     flow = new TennentsFlow();
     await flow.loadModels();
     socket = new TennentsFlowSocket(flow);
-    setInterval(() => socket.sendNextStep(), 2200);
+    let trigger = () => {
+        socket.sendNextStep();
+        setTimeout(trigger, tickPeriod);
+    }
+
+    setTimeout(trigger, tickPeriod);
+
+    // buttons
+
+    document.querySelector("#slowDown").addEventListener("click", () => {
+        setTickPeriod(tickPeriod + 500);
+    });
+
+    document.querySelector("#speedUp").addEventListener("click", () => {
+        if (tickPeriod <= 500) return;
+        setTickPeriod(tickPeriod - 500);
+    });
+
+    document.querySelector("#playPause").addEventListener("click", () => {
+
+    });
+
 }
 
 else {
